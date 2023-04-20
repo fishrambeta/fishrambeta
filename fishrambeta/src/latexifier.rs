5edf98bc9f28044337b9a7576b715c008ff2af0e
Jcuhfehl <jesper@justgamingltd.nl>
2023-04-20 14:17:49 +0200

    Finish equation to latex converter with all current operations

--- fishrambeta/src/latexifier.rs
+++ fishrambeta/src/latexifier.rs
@@ -6,8 +6,6 @@ use std::fmt;
 impl fmt::Display for Equation {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", equation_to_latex(self.clone()))
-        // or, alternatively:
-        // fmt::Debug::fmt(self, f)
     }
 }
 
@@ -15,10 +13,10 @@ fn equation_to_latex(equation: Equation) -> String {
     match equation {
         Equation::Variable(variable) => return to_latex_variable(variable),
         Equation::Addition(addition) => return to_latex_addition(addition),
-        // Equation::Subtraction(subtraction) => return to_latex_subtraction(subtraction),
-        // Equation::Multiplication(multiplication) => return to_latex_multiplication(multiplication),
-        // Equation::Division(division) => return to_latex_division(division),
-        // Equation::Power(power) => return to_latex_power(power),
+        Equation::Subtraction(subtraction) => return to_latex_subtraction(subtraction),
+        Equation::Multiplication(multiplication) => return to_latex_multiplication(multiplication),
+        Equation::Division(division) => return to_latex_division(*division),
+        Equation::Power(power) => return to_latex_power(*power),
         _ => return "".to_string(),
     }
 }
@@ -39,12 +37,41 @@ fn to_latex_addition(addition: Vec<Equation>) -> String {
     for term in addition {
         latex.push_str(&format!("{}+", term));
     }
+    latex.pop();
+    return latex;
+}
+
+fn to_latex_subtraction(subtraction: Vec<Equation>) -> String {
+    let mut latex: String = "".to_string();
+    for term in subtraction {
+        latex.push_str(&format!("{}-", term));
+    }
+    latex.pop();
     return latex;
 }
 
 fn to_latex_constant(constant: Constant) -> String {
     match constant {
         Constant::E => return "e".to_string(),
-        Constant::PI => return "\\pi".to_string()
+        Constant::PI => return "\\pi".to_string(),
     }
+}
+
+fn to_latex_multiplication(multiplication: Vec<Equation>) -> String {
+    let mut latex: String = "".to_string();
+    for term in multiplication {
+        latex.push_str(&format!("{}*", term));
+    }
+    latex.pop();
+    return latex;
+}
+
+fn to_latex_power(power: (Equation, Equation)) -> String {
+    let latex: String = format!("{}^{{{}}}", power.0, power.1);
+    return latex;
+}
+
+fn to_latex_division(division: (Equation, Equation)) -> String {
+    let latex: String = format!("\\frac{{{}}}{{{}}}", division.0, division.1);
+    return latex;
 }
