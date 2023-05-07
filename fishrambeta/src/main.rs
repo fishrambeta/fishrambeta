96e055f2262c559a8820754215f573078351ce52
danielvisser333 <danielvisser10@outlook.com>
2023-05-07 15:06:42 +0200

    Add IR for LaTeX parser

--- fishrambeta/src/main.rs
+++ fishrambeta/src/main.rs
@@ -1,20 +1,24 @@
 mod math;
-mod latexifier;
 mod parser;
+mod logger;
 
 use clap::Parser;
-use crate::math::{Constant, Equation, Symbol, Variable};
 
 #[derive(Parser, Debug)]
 pub struct Args{
     //The equation to solve formatted in LaTeX
     #[arg(short, long)]
     equation : String,
+    #[clap(flatten)]
+    verbose: clap_verbosity_flag::Verbosity,
+    #[arg(short, long)]
+    log_out: Option<String>,
 }
 
 fn main() {
     let args = Args::parse();
-    let equation = parser::to_equation(args.equation);
-    let simplified = equation.simplify().simplify().simplify();
-    println!("{}", simplified.to_string());
+    let logger = logger::new(args.log_out, args.verbose);
+    let equation = parser::to_equation(args.equation, &logger);
+    //let simplified = equation.simplify().simplify().simplify();
+    //println!("{}", simplified.to_string());
 }
--- fishrambeta/src/parser.rs
+++ fishrambeta/src/parser.rs
@@ -1,60 +1,171 @@
-use crate::math::Constant;
 use crate::math::Equation;
-use crate::math::Variable;
-use crate::math::Variable::Letter;
+use slog::{crit, Logger};
 
-pub fn to_equation(equation_string: String) -> Equation {
-    if equation_string.contains("*") {
-        let sub_equations_string = equation_string.split('*');
-        let sub_equations_strings = sub_equations_string.collect::<Vec<&str>>();
-
-        let mut sub_equations: Vec<Equation> = Vec::new();
-        for sub_equation_string in sub_equations_strings {
-            sub_equations.push(to_equation(sub_equation_string.to_string()));
+#[derive(Debug, Clone)]
+struct LatexEqnIR {
+    name: String,
+    parameters: Vec<LatexEqnIR>,
+    #[cfg(debug_assertions)]
+    depth: u32,
+}
+impl LatexEqnIR {
+    pub fn create_from_latex(
+        latex: Vec<char>,
+        logger: &Logger,
+        #[cfg(debug_assertions)] depth: u32,
+    ) -> Self {
+        let mut latex = latex;
+        //Handle equals sign
+        return if latex.contains(&'='){
+            let index = unsafe{latex.iter().position(|char| char == &'=').unwrap_unchecked()};
+            let (left, right) = latex.split_at(index);
+            let (left, mut right) = (left.to_vec(), right.to_vec());
+            right.remove(0);
+            if left.contains(&'=') || right.contains(&'=') {
+                crit!(logger, "Equation contains multiple equals signs, unsupported");
+                panic!();
+            }
+            #[cfg(debug_assertions)]
+            let (left_ir, right_ir) = (Self::create_from_latex(left, logger, depth+1), Self::create_from_latex(right, logger, depth+1));
+            #[cfg(not(debug_assertions))]
+            let (left_ir, right_ir) = (Self::create_from_latex(left.to_vec(), logger), Self::create_from_latex(right.to_vec(), logger));
+            return Self{
+                name: String::from("equals"),
+                parameters: vec!(left_ir, right_ir),
+                #[cfg(debug_assertions)] depth,
+            };
         }
-        return Equation::Multiplication(sub_equations);
-    }
-
-    if equation_string.contains("/") {
-        let sub_equations_string = equation_string.split('/');
-        let sub_equations_strings = sub_equations_string.collect::<Vec<&str>>();
-        return Equation::Division(Box::new((
-            to_equation(sub_equations_strings[0].to_string()),
-            to_equation(sub_equations_strings[1].to_string()),
-        )));
-    }
-
-    if equation_string.contains("+") {
-        let sub_equations_string = equation_string.split('+');
-        let sub_equations_strings = sub_equations_string.collect::<Vec<&str>>();
-
-        let mut sub_equations: Vec<Equation> = Vec::new();
-        for sub_equation_string in sub_equations_strings {
-            sub_equations.push(to_equation(sub_equation_string.to_string()));
+        //Handle \*****{}{}{}{}{}... constructions
+        else if latex.starts_with(&['\\']) && latex.contains(&'{') && latex.contains(&'}') {
+            let mut name = vec![];
+            latex.remove(0);
+            loop {
+                if latex[0] == '{' {
+                    break;
+                } else {
+                    name.push(latex.remove(0))
+                }
+            }
+            let name: String = name.iter().collect();
+            let mut parameters = vec![];
+            //Loop over individual parameters
+            loop {
+                if latex.len() == 0{
+                    break;
+                }
+                if latex[0] != '{' {
+                    break;
+                }
+                if latex.len() == 0 {
+                    break;
+                }
+                latex.remove(0); //Remove trailing {
+                let mut depth_counter = 1;
+                let mut inner_data = vec![];
+                loop {
+                    // Loop until }
+                    if latex[0] == '{' {
+                        depth_counter += 1
+                    }
+                    else if latex[0] == '}' {
+                        depth_counter += -1
+                    }
+                    if depth_counter == 0 {
+                        latex.remove(0);
+                        break;
+                    }
+                    inner_data.push(latex.remove(0))
+                }
+                #[cfg(debug_assertions)]
+                let parameter = LatexEqnIR::create_from_latex(inner_data, logger, depth + 1);
+                #[cfg(not(debug_assertions))]
+                let parameter = LatexEqnIR::create_from_latex(inner_data, logger);
+                parameters.push(parameter);
+            }
+            Self {
+                name,
+                parameters,
+                #[cfg(debug_assertions)]
+                depth,
+            }
         }
-        return Equation::Addition(sub_equations);
+        //Handle +- and other operators
+        else if latex.contains(&'+') {
+            let index = unsafe {
+                latex
+                    .iter()
+                    .position(|char| char == &'+')
+                    .unwrap_unchecked()
+            };
+            let (first, second) = latex.split_at(index);
+            let (first, mut second) = (first.to_vec(), second.to_vec());
+            second.remove(0);
+            #[cfg(debug_assertions)]
+            let (first_ir, second_ir) = (
+                Self::create_from_latex(first, logger, depth + 1),
+                Self::create_from_latex(second, logger, depth + 1),
+            );
+            #[cfg(not(debug_assertions))]
+            let (first_ir, second_ir) = (
+                Self::create_from_latex(first, logger),
+                Self::create_from_latex(second, logger),
+            );
+            Self {
+                name: String::from("subtraction"),
+                parameters: vec![first_ir, second_ir],
+                #[cfg(debug_assertions)]
+                depth,
+            }
+        }
+        else if latex.contains(&'-') {
+            let index = unsafe {
+                latex
+                    .iter()
+                    .position(|char| char == &'-')
+                    .unwrap_unchecked()
+            };
+            let (first, second) = latex.split_at(index);
+            let (first, mut second) = (first.to_vec(), second.to_vec());
+            second.remove(0);
+            #[cfg(debug_assertions)]
+            let (first_ir, second_ir) = (
+                Self::create_from_latex(first, logger, depth + 1),
+                Self::create_from_latex(second, logger, depth + 1),
+            );
+            #[cfg(not(debug_assertions))]
+            let (first_ir, second_ir) = (
+                Self::create_from_latex(first, logger),
+                Self::create_from_latex(second, logger),
+            );
+            Self {
+                name: String::from("addition"),
+                parameters: vec![first_ir, second_ir],
+                #[cfg(debug_assertions)]
+                depth,
+            }
+        }
+        //Constants
+        else {
+            Self {
+                name: latex.iter().collect(),
+                parameters: vec![],
+                #[cfg(debug_assertions)]
+                depth,
+            }
+        };
     }
-
-    if equation_string.contains("^") {
-        let sub_equations_string = equation_string.split('^');
-        let sub_equations_strings = sub_equations_string.collect::<Vec<&str>>();
-        return Equation::Power(Box::new((
-            to_equation(sub_equations_strings[0].to_string()),
-            to_equation(sub_equations_strings[1].to_string()),
-        )));
+    pub fn ir_to_eqn(&self, logger: &Logger) -> Equation {
+        todo!()
     }
-
-    return only_letters_to_equation(equation_string);
 }
 
-pub fn only_letters_to_equation(letters: String) -> Equation {
-    let mut variables: Vec<Equation> = Vec::new();
-    for (_i, c) in letters.chars().enumerate() {
-        match c {
-            'e' | 'E' => variables.push(Equation::Variable(Variable::Constant(Constant::E))),
-            _ => variables.push(Equation::Variable(Letter(c.to_string()))),
-        }
-    }
-
-    return Equation::Multiplication(variables);
+pub fn to_equation(latex: String, logger: &Logger) -> Equation {
+    let ir = latex_to_ir(latex, logger);
+    return ir_to_eqn(ir, logger);
+}
+fn latex_to_ir(latex: String, logger: &Logger) -> LatexEqnIR {
+    return LatexEqnIR::create_from_latex(latex.chars().filter(|char| char != &' ').collect(), logger, 0);
+}
+fn ir_to_eqn(ir: LatexEqnIR, logger: &Logger) -> Equation {
+    return ir.ir_to_eqn(logger);
 }
