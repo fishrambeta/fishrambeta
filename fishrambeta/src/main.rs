60bc91d9e95b21e5688c09c52aa1d6064e10f424
Jcuhfehl <jesper@justgamingltd.nl>
2023-05-12 21:39:02 +0200

    Add operation to be performed

--- fishrambeta/src/main.rs
+++ fishrambeta/src/main.rs
@@ -1,20 +1,38 @@
-mod math;
 mod latexifier;
+mod math;
 mod parser;
 
-use clap::Parser;
 use crate::math::{Constant, Equation, Symbol, Variable};
+use clap::Parser;
+use clap::ValueEnum;
 
 #[derive(Parser, Debug)]
-pub struct Args{
+pub struct Args {
     //The equation to solve formatted in LaTeX
     #[arg(short, long)]
-    equation : String,
+    equation: String,
+    //The thing to do with the equation
+    #[arg(short, long, value_enum)]
+    operation: Operation,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
+enum Operation {
+    Simplify,
+    Solve,
+    Calculate
 }
 
 fn main() {
     let args = Args::parse();
     let equation = parser::to_equation(args.equation);
-    let simplified = equation.simplify().simplify().simplify();
-    println!("{}", simplified.to_string());
+    let result = process_operation(equation, args.operation);
+    println!("{}", result.to_string());
+}
+
+fn process_operation(equation: Equation, operation: Operation) -> Equation{
+    match operation {
+        Operation::Simplify => {return equation.simplify().simplify().simplify()}
+        _ => {panic!("Operation not yet supported")}
+    }
 }
