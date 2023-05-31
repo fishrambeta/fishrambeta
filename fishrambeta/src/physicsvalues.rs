fe4518ae21ba3753569fc387c1ed51b669317352
Jcuhfehl <jesper@justgamingltd.nl>
2023-05-31 15:11:23 +0200

    Fix mathquill editing for fishrambeta-web

--- fishrambeta/src/physicsvalues.rs
+++ fishrambeta/src/physicsvalues.rs
@@ -4,5 +4,6 @@ use std::{collections::HashMap, hash::Hash};
 pub fn physics_values() -> HashMap<Variable, f64> {
     let mut values = HashMap::new();
     values.insert(Variable::Letter("g".to_string()), 9.81);
+    values.insert(Variable::Letter("\\hbar".to_string()), 1.054571817e-34);
     return values;
 }
