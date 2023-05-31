6af161d512ac1d6f886337fc251e3e8ec0bc55d8
Jcuhfehl <jesper@justgamingltd.nl>
2023-05-31 13:44:53 +0200

    Add physicsvalues for easier calculation

--- fishrambeta-wasm/src/lib.rs
+++ fishrambeta-wasm/src/lib.rs
@@ -1,12 +1,22 @@
+use fishrambeta::{self, math::Symbol, physicsvalues};
+use slog::{o, Discard, Logger};
 use wasm_bindgen::prelude::*;
-use fishrambeta;
-use slog::{Logger,Discard,o};
 
 #[wasm_bindgen]
-pub fn simplify(equation: &str) -> String{
-    let logger = Logger::root(Discard,o!());
+pub fn simplify(equation: &str) -> String {
+    let logger = Logger::root(Discard, o!());
     let parsed = fishrambeta::parser::to_equation(equation.to_string(), &logger);
-    let parsed_back = fishrambeta::parser::to_latex(parsed, &logger);
-    return "test".to_string();
-    //return parsed_back;
+    let simplified = parsed.simplify().simplify();
+    let parsed_back = fishrambeta::parser::to_latex(simplified, &logger);
+    return parsed_back;
+}
+
+#[wasm_bindgen]
+pub fn calculate(equation: &str) -> f64 {
+    let logger = Logger::root(Discard, o!());
+    let values = physicsvalues::physics_values();
+    let parsed: fishrambeta::math::Equation =
+        fishrambeta::parser::to_equation(equation.to_string(), &logger);
+    let result = parsed.calculate(&values);
+    return result;
 }
