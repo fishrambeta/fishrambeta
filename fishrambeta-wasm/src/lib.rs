01011d11e2c3c8e5b40d51f72adbd0e3e7b4fca5
Jcuhfehl <jesper@justgamingltd.nl>
2023-06-02 14:51:14 +0200

    Add user defined values to calculate in web

--- fishrambeta-wasm/src/lib.rs
+++ fishrambeta-wasm/src/lib.rs
@@ -1,5 +1,10 @@
-use fishrambeta::{self, math::Symbol, physicsvalues};
+use fishrambeta::{
+    self,
+    math::{Equation, Symbol, Variable},
+    physicsvalues,
+};
 use slog::{o, Discard, Logger};
+use std::{collections::HashMap, hash::Hash};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -12,11 +17,24 @@ pub fn simplify(equation: &str) -> String {
 }
 
 #[wasm_bindgen]
-pub fn calculate(equation: &str) -> f64 {
+pub fn calculate(equation: &str, user_values_keys: &str, user_values_values: &[f64]) -> f64 {
     let logger = Logger::root(Discard, o!());
-    let values = physicsvalues::physics_values();
+    let mut values = physicsvalues::physics_values();
+    let user_values_hashmap = user_values_to_hashmap(
+        user_values_keys.split("\\n\\n").collect::<Vec<_>>(),
+        user_values_values,
+    );
+    values.extend(user_values_hashmap);
     let parsed: fishrambeta::math::Equation =
         fishrambeta::parser::to_equation(equation.to_string(), &logger);
     let result = parsed.calculate(&values);
     return result;
+}
+
+fn user_values_to_hashmap(keys: Vec<&str>, values: &[f64]) -> HashMap<Variable, f64> {
+    let mut values_hashmap = HashMap::new();
+    for (key, value) in keys.iter().zip(values.iter()) {
+        values_hashmap.insert(Variable::Letter(key.to_string()), *value);
+    }
+    return values_hashmap;
 }
