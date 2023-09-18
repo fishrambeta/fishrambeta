e14a372b711bb7a626553d551bceee7b3b2e1e48
Jcuhfehl <jesper@justgamingltd.nl>
2023-09-18 18:47:33 +0200

    Implement display for equation with to_latex()

--- fishrambeta/src/math/to_latex.rs
+++ fishrambeta/src/math/to_latex.rs
@@ -1,8 +1,15 @@
 use crate::math::Equation;
 use crate::parser::IR;
+use std::fmt;
 
 impl Equation {
     pub fn to_latex(self: &Self) -> String {
         return IR::equation_to_latex(self.clone(), false);
+    }
+}
+
+impl fmt::Display for Equation {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.to_latex())
     }
 }
