748d7e3c7d91b16fe2d4e2cf5bfe9bf67d0437eb
Jcuhfehl <jesper@justgamingltd.nl>
2023-09-25 18:36:37 +0200

    Integrate powers of x

--- fishrambeta/src/math/integrate/mod.rs
+++ fishrambeta/src/math/integrate/mod.rs
@@ -1,16 +1,24 @@
+use num_rational::Rational64;
+
 use super::{Equation, Variable};
 
 impl Equation {
     pub fn integrate(&self, integrate_to: &Variable) -> Equation {
-        let fixed_terms = self.get_factors();
-        let mut equation_to_integrate: Equation = (*self).clone();
+        let mut equation_to_integrate: Equation = (*self).clone().simplify();
+        let fixed_terms = equation_to_integrate.get_factors();
         let mut integrated_equation = Vec::new();
 
         for fixed_term in fixed_terms.into_iter() {
+            println!(
+                "{} has factor? {} is constant? {}",
+                fixed_term,
+                equation_to_integrate.has_factor(&fixed_term),
+                fixed_term.term_is_constant(integrate_to)
+            );
             if equation_to_integrate.has_factor(&fixed_term)
                 && fixed_term.term_is_constant(integrate_to)
             {
-                equation_to_integrate = equation_to_integrate.remove_factor(&fixed_term);
+                equation_to_integrate = equation_to_integrate.remove_factor(&fixed_term).simplify();
                 integrated_equation.push(fixed_term);
             }
         }
@@ -21,6 +29,7 @@ impl Equation {
     }
 
     fn do_integration(&self, integrate_to: &Variable) -> Equation {
+        println!("Hard integrating: {}", self);
         match self {
             Equation::Addition(addition) => {
                 return Equation::Addition(
@@ -37,6 +46,33 @@ impl Equation {
                 return Equation::Multiplication(vec![
                     Equation::Variable(Variable::Rational(*r)),
                     Equation::Variable(integrate_to.clone()),
+                ])
+            }
+            Equation::Variable(v) if v == integrate_to => {
+                return Equation::Multiplication(vec![
+                    Equation::Variable(Variable::Rational(Rational64::new(1, 2))),
+                    Equation::Power(Box::new((
+                        Equation::Variable(integrate_to.clone()),
+                        Equation::Variable(Variable::Integer(2)),
+                    ))),
+                ])
+            }
+            Equation::Power(box (b, n)) if *b == Equation::Variable(integrate_to.clone()) => {
+                return Equation::Multiplication(vec![
+                    Equation::Division(Box::new((
+                        Equation::Variable(Variable::Integer(1)),
+                        Equation::Addition(vec![
+                            n.clone(),
+                            Equation::Variable(Variable::Integer(1)),
+                        ]),
+                    ))),
+                    Equation::Power(Box::new((
+                        Equation::Variable(integrate_to.clone()),
+                        Equation::Addition(vec![
+                            n.clone(),
+                            Equation::Variable(Variable::Integer(1)),
+                        ]),
+                    ))),
                 ])
             }
             _ => todo!(),
