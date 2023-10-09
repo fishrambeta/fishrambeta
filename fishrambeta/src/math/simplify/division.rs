c3416107754e25d918e01e8ce4fafbdb2dafa7ef
Jcuhfehl <jesper@justgamingltd.nl>
2023-10-09 13:24:13 +0200

    Improve division simplification

--- fishrambeta/src/math/simplify/division.rs
+++ fishrambeta/src/math/simplify/division.rs
@@ -4,6 +4,27 @@ pub(super) fn simplify_division(division: Box<(Equation, Equation)>) -> Equation
     let mut numerator = division.0.simplify();
     let mut denominator = division.1.simplify();
 
+    match numerator {
+        Equation::Division(division) => {
+            return Equation::Division(Box::new((
+                division.0,
+                Equation::Multiplication(vec![division.1, denominator]),
+            )))
+            .simplify();
+        }
+        _ => {}
+    }
+    match denominator {
+        Equation::Division(division) => {
+            return Equation::Division(Box::new((
+                Equation::Multiplication(vec![numerator, division.1]),
+                division.0,
+            )))
+            .simplify();
+        }
+        _ => {}
+    }
+
     for factor in denominator.shared_factors(&numerator) {
         if (&numerator).has_factor(&factor) && (&denominator).has_factor(&factor) {
             numerator = numerator.remove_factor(&factor);
