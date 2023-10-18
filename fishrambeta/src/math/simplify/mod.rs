6c3aeade81e6eaea6e715eaf57af159726913a4e
Jcuhfehl <jesper@justgamingltd.nl>
2023-10-18 22:48:01 +0200

    Fix ln(0) derivative

--- fishrambeta/src/math/simplify/mod.rs
+++ fishrambeta/src/math/simplify/mod.rs
@@ -9,9 +9,8 @@ impl Equation {
     pub fn simplify_until_complete(self) -> Self {
         let mut equation = self.clone();
         let mut previous = equation.to_latex();
-        for i in 1..3 {
+        for _ in 1..5 {
             equation = equation.simplify();
-            //println!("{}: {}", i, equation);
             if equation.to_latex() == previous {
                 break;
             }
