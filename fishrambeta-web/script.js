4e6c795beda869e4772b37066bc2a025c244f8d0
Freya Winters <freya@gosan.nl>
2024-08-18 23:43:42 +0200

    Add values to web

--- fishrambeta-web/script.js
+++ fishrambeta-web/script.js
@@ -15,8 +15,29 @@ function on_input_changed() {
   process_operation();
 }
 
-function on_operation_changed() {
+window.on_operation_changed = function () {
   process_operation();
+};
+
+function get_values() {
+  var keys = [];
+  var values = [];
+  console.log(value_fields);
+  for (var i = 0; i < value_fields.length; i++) {
+    var key_latex = value_fields[i].key.latex();
+    var value_latex = value_fields[i].value.latex();
+    if (
+      key_latex != "" &&
+      value_latex != "" &&
+      key_latex != null &&
+      value_latex != null
+    ) {
+      var value = calculate(value_latex, "", []);
+      keys.push(key_latex);
+      values.push(value);
+    }
+  }
+  return { keys: keys, values: values };
 }
 
 function process_operation() {
@@ -34,7 +55,9 @@ function process_operation() {
         result = simplify(input);
         break;
       case "calculate":
-        result = calculate(input);
+        var values = get_values();
+        console.log(values);
+        result = calculate(input, values.keys.join("\\n\\n"), values.values);
         break;
       case "differentiate":
         result = differentiate(input);
@@ -46,9 +69,45 @@ function process_operation() {
 
     result_mathfield.latex(result);
   } catch (error) {
+    console.log(error);
     result_mathfield.latex("\\textbf{Invalid LaTeX (" + error + ")}");
   }
 }
+
+function add_new_value_field(id) {
+  var container = document.getElementById("input-values-container");
+  var key_id = `input-value-key-${id}`;
+  var value_id = `input-value-value-${id}`;
+  container.insertAdjacentHTML(
+    "afterbegin",
+    `<p><span class="latex-key" id="${key_id}"></span>=<span class="latex-value" id="${value_id}"></span></p>`,
+  );
+  var key_span = document.getElementById(key_id);
+  var key_mathfield = MQ.MathField(key_span, {
+    spaceBehavesLikeTab: true,
+    handlers: {
+      edit: function () {
+        try {
+          process_operation();
+        } catch {}
+      },
+    },
+  });
+  var value_span = document.getElementById(value_id);
+  var value_mathfield = MQ.MathField(value_span, {
+    spaceBehavesLikeTab: true,
+    handlers: {
+      edit: function () {
+        try {
+          process_operation();
+        } catch {}
+      },
+    },
+  });
+  value_fields[id] = { key: key_mathfield, value: value_mathfield };
+}
+
+var value_fields = [];
 
 var MQ = MathQuill.getInterface(2);
 var input_span = document.getElementById("latex-input");
@@ -81,3 +140,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // Add an event listener to the select element
   selectElement.addEventListener("change", toggleVisibility);
 });
+
+for (let i = 0; i < 10; i++) {
+  add_new_value_field(i);
+}
